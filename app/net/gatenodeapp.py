cb4f8968a90d9cb1a6a286a7363e58f4005bd74a
xiaomi2019 <364786954@qq.com>
2019-03-04 14:16:52 +0800

    debug

--- app/net/gatenodeapp.py
+++ app/net/gatenodeapp.py
@@ -10,18 +10,18 @@ from twisted.python import log
 @remoteserviceHandle('gate')
 def pushObject(topicID,msg,sendList):
 	print "net pushObject %s %s"%(topicID,sendList);
-    GlobalObject().netfactory.pushObject(topicID, msg, sendList)
+	GlobalObject().netfactory.pushObject(topicID, msg, sendList);
 
 @remoteserviceHandle('gate')
 def pushObjectOthers(topicID,msg,exclude_list):
-    lis = GlobalObject().netfactory.connmanager._connections.keys()     #获取所有在线用户
-    for i in exclude_list:
-        if i in lis:
-            lis.remove(i) 
-    GlobalObject().netfactory.pushObject(topicID, msg, lis);
+	lis = GlobalObject().netfactory.connmanager._connections.keys()     #获取所有在线用户
+	for i in exclude_list:
+		if i in lis:
+			lis.remove(i) 
+	GlobalObject().netfactory.pushObject(topicID, msg, lis);
 
 
 @remoteserviceHandle('gate')
 def loseConnect(id):
-    log.msg("net loseConnect ",id);
-    GlobalObject().netfactory.loseConnection(id);
+	log.msg("net loseConnect ",id);
+	GlobalObject().netfactory.loseConnection(id);
